3ceb7b67c9249fd9bd8f0fd07b4e55d86797c6ed
Mateus <matt_ufo2@hotmail.com>
2026-05-18 16:57:07 -0300

    feat: adicionar testes de unidade para todos os cenários do validador de usuário

--- tests/Validators.Testes/User/Register/RegisterUserValidatorTests.cs
+++ tests/Validators.Testes/User/Register/RegisterUserValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
 using Shouldly;
 
 namespace Validators.Testes.User.Register;
@@ -16,5 +17,85 @@ public class RegisterUserValidatorTests
         var result = validator.Validate(request);
 
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_NameIsEmpty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count.ShouldBe(1);
+            erros.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_EmailIsEmpty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count.ShouldBe(1);
+            erros.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_PasswordIsEmpty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count.ShouldBe(1);
+            erros.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_EmailIsInvalid()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        
+        request.Email = "invalid-email";
+        
+        var validator = new RegisterUserValidator();
+        
+        var result = validator.Validate(request);
+        
+        result.IsValid.ShouldBeFalse();
+        
+        result.Errors.ShouldSatisfyAllConditions(erros =>
+        {
+            erros.Count.ShouldBe(1);
+            erros.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
     }
 }
