662bbbc34583242c08e9a3ec7656db603313e00a
Mateus <matt_ufo2@hotmail.com>
2026-05-18 12:24:17 -0300

    feat: Intruducao aos testes de unidade usando o xUnit

---
+++ tests/Validators.Testes/User/Register/RegisterUserValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requests;
+
+namespace Validators.Testes.User.Register;
+
+public class RegisterUserValidatorTests
+{
+    [Fact]
+    public void Sucess() //AAA A = Arange A = Act A = Assert
+    {
+        var request = new RequestRegisterUserJson
+        {
+            Name = "John Doe",
+            Email = "john.doe@gmail.com",
+            Password = "password123"
+        };
+
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+    }
+}
