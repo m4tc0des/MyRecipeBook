e96a620a71833f0b9d2f757b3ac193c1257097ea
Mateus <matt_ufo2@hotmail.com>
2026-06-16 11:08:35 -0300

    feat: Adiciona estrutura básica da classe JwtTokenHandler

---
+++ src/Backend/MyRecipeBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -0,0 +1,20 @@
+﻿using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Security.Tokens;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens.Access;
+
+internal sealed class JwtTokenHandler : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenHandler(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+    public string Generate(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
