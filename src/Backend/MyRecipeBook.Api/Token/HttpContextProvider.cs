c7a8f930be3b5a77c0c449e446bb09dae8457b25
Mateus <matt_ufo2@hotmail.com>
2026-06-25 10:00:02 -0300

    feat(security): introduz IAccessTokenProvider para abstrair recuperação do token
    Cria a interface IAccessTokenProvider na camada de Domínio.
    
    Implementa a classe HttpContextProvider na camada de API utilizando IHttpContextAccessor.
    
    Permite a extração desacoplada do token do cabeçalho Authorization a partir do contexto HTTP atual.

---
+++ src/Backend/MyRecipeBook.Api/Token/HttpContextProvider.cs
@@ -0,0 +1,20 @@
+﻿using MyRecipeBook.Domain.Security.Tokens;
+
+namespace MyRecipeBook.Api.Token;
+
+internal sealed class HttpContextProvider : IAccessTokenProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public HttpContextProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public string GetToken()
+    {
+        var accessToken = _httpContextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+        return accessToken;
+    }
+}
