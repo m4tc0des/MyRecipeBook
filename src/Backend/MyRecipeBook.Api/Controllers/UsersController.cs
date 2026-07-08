c28437a252537ca2c2064555dc02a8fa3a24a0b9
Mateus <matt_ufo2@hotmail.com>
2026-07-08 15:00:19 -0300

    feat: adiciona fluxo de atualização de perfil e endpoint de alteração de senha
    - Cria o UpdateUserUseCase com validações de e-mail duplicado e regras do FluentValidation.
    - Adiciona o contrato IUpdateUserUseCase e o validador UpdateUserValidator.
    - Expõe os endpoints PUT "profile" e "password" na UserController com retornos HTTP 204 NoContent.

--- src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.ChangePassword;
 using MyRecipeBook.Application.UseCases.User.Profile;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Application.UseCases.User.Update;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -29,6 +31,30 @@ namespace MyRecipeBook.Api.Controllers
             var result = await useCase.Execute();
 
             return Ok(result);
+        }
+
+        [HttpPut]
+        [Authorize]
+        [Route("profile")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateProfile([FromServices] IUpdateUserUseCase useCase, [FromBody] RequestUpdateUserJson request)
+        {
+            await useCase.Execute(request);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Authorize]
+        [Route("password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdatePassword([FromServices] IChangePasswordUseCase useCase, [FromBody] RequestChangePasswordJson request)
+        {
+            await useCase.Execute(request);
+
+            return NoContent();
         }
     }
 }
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Update/UpdateUserUseCase.cs
@@ -0,0 +1,63 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Update;
+
+public class UpdateUserUseCase : IUpdateUserUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IUserUpdateOnlyRepository _userUpdateOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateUserUseCase(ILoggedUser loggedUser, IUserReadOnlyRepository userReadOnlyRepository, IUserUpdateOnlyRepository userUpdateOnlyRepository, IUnitOfWork unitOfWork)
+    {
+        _loggedUser = loggedUser;
+        _userReadOnlyRepository = userReadOnlyRepository;
+        _userUpdateOnlyRepository = userUpdateOnlyRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(RequestUpdateUserJson request)
+    {
+        var loggedUser = await _loggedUser.Get();
+
+        await Validate(request, loggedUser);
+
+        loggedUser.Name = request.Name;
+
+        loggedUser.Email = request.Email;
+
+        _userUpdateOnlyRepository.UpdateProfile(loggedUser);
+
+        await _unitOfWork.Commit();
+    }
+
+    private async Task Validate(RequestUpdateUserJson request, Domain.Entities.User loggedUser)
+    {
+        var validator = new UpdateUserValidator();
+
+        var result = validator.Validate(request);
+
+        if (loggedUser.Email.Equals(request.Email) == false)
+        {
+            var userExist = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+            if (userExist)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure("Email", ResourceMessagesException.VALIDATION_EMAIL_ALREADY_EXISTS));
+            }
+        }
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
