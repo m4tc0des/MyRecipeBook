0d1fdad4157cd216c1571334024c0d04bbe28c59
Mateus <matt_ufo2@hotmail.com>
2026-07-14 17:58:00 -0300

    feat(receitas): adiciona controller e classe de request para cadastro de receitas

---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRecipeJson.cs
@@ -0,0 +1,13 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Requests;
+
+public class RequestRecipeJson
+{
+    public string Title { get; set; } = string.Empty;
+    public CookTime CookTime { get; set; }
+    public IList<string> Ingredients { get; set; } = new List<string>();
+    public IList<RequestRecipeInstructionJson> Instructions { get; set; } = new List<RequestRecipeInstructionJson>();
+    public IList<DishTypes> DishTypes { get; set; } = new List<DishTypes>();
+
+}
