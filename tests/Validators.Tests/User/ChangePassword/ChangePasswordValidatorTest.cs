b2a36b12635a7cea05e9fe6d74ad6e5e5e613ed0
Mateus <matt_ufo2@hotmail.com>
2026-07-20 11:15:05 -0300

    test: adiciona testes unitarios e de integracao para alteracao de senha

---
+++ tests/Validators.Tests/User/ChangePassword/ChangePasswordValidatorTest.cs
@@ -0,0 +1,65 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.ChangePassword;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+
+namespace Validators.Tests.User.ChangePassword;
+
+public class ChangePasswordValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new ChangePasswordValidator();
+
+        var request = RequestChangePasswordJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_When_NewPasswordIsEmpty()
+    {
+        var validator = new ChangePasswordValidator();
+
+        var request = RequestChangePasswordJsonBuilder.Build();
+
+        request.NewPassword = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Validate_ShouldHaveError_When_NewPasswordIsLessThan6Characters(int passwordLength)
+    {
+        var validator = new ChangePasswordValidator();
+
+        var request = RequestChangePasswordJsonBuilder.Build(passwordLength);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_MIN_LENGTH));
+        });
+    }
+
+}
---
+++ tests/WebApi.Tests/User/ChangePassword/ChangePasswordTests.cs
@@ -0,0 +1,66 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Extensions;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Tests;
+using WebApi.Tests.InlineData;
+using WebApi.Tests.Resources;
+
+namespace Validators.Tests.User.ChangePassword;
+
+public class ChangePasswordTests : BaseIntegrationTest
+{
+    private const string REQUEST_URI = "users/password";
+
+    private readonly UserIdentityManager _userOne;
+
+    public ChangePasswordTests(MyRecipeBookApplicationFactory factory) : base(factory)
+    {
+        _userOne = factory.User_One;
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestChangePasswordJsonBuilder.Build();
+
+        request.CurrentPassword = _userOne.GetPassword();
+
+        var response = await Put(REQUEST_URI, request, accessToken: _userOne.GetAccessToken());
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineData))]
+    public async Task Error_NewPassword_Empty(string culture)
+    {
+        var request = new RequestChangePasswordJson
+        {
+            CurrentPassword = _userOne.GetPassword(),
+            NewPassword = string.Empty
+        };
+
+        var response = await Put(REQUEST_URI, request, accessToken: _userOne.GetAccessToken(), culture);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("VALIDATION_PASSWORD_REQUIRED", new CultureInfo(culture));
+
+        errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count().ShouldBe(1);
+            errors.ShouldContain(error => error.GetString().IsNotEmpty() && error.GetString()!.Equals(expectedMessage));
+        });
+    }
+}
