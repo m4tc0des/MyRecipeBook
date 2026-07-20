019339122e401cb5720e30363e9c00e6b22c2041
Mateus <matt_ufo2@hotmail.com>
2026-07-20 11:21:08 -0300

    test: adiciona validadores de receita e usuario e silencia warning NU1903

---
+++ tests/Validators.Tests/User/Update/UpdateUserValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Update;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Validators.Tests.User.Update;
+
+public class UpdateUserValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("               ")]
+    [SuppressMessage("Usage", "xUnit1012: Null should only be used for nullable parameters", Justification = "Intentionally testing null value")]
+    public void Validation_ShouldHaveError_When_NameIsEmpty(string name)
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        request.Name = name;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("               ")]
+    [SuppressMessage("Usage", "xUnit1012: Null should only be used for nullable parameters", Justification = "Intentionally testing null value")]
+    public void Validation_ShouldHaveError_When_EmailIsEmpty(string email)
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        request.Email = email;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validation_ShouldHaveError_When_EmailIsInvalid()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        request.Email = "email.com";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
+    }
+}
