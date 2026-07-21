6a3cdf66b86dd72a73d185482031b2f98b9196a6
Mateus <matt_ufo2@hotmail.com>
2026-07-21 18:01:16 -0300

    feat(domain): adiciona entidade Recipe
    Cria a entidade Recipe herdando de EntityBase
    
    Adiciona relacionamentos para ingredientes, instruções e tipos de prato
    
    Inclui vínculo com UserId e propriedades básicas de receita

---
+++ src/Backend/MyRecipeBook.Domain/Entities/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Entities;
+
+public class Recipe : EntityBase
+{
+    public string Title { get; set; } = string.Empty;
+    public ICollection<RecipeIngredient> Ingredients { get; set; } = new List<RecipeIngredient>();
+    public ICollection<RecipeInstruction> Instructions{ get; set; } = new List<RecipeInstruction>();
+    public ICollection<RecipeDishType> DishTypes{ get; set; } = new List<RecipeDishType>();
+    public CookTime CookTime { get; set; }
+    public Guid UserId { get; set; }
+}
