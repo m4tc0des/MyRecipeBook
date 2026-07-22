13585fe7821168a5a2c9c160590f674bf8ffa674
Mateus <matt_ufo2@hotmail.com>
2026-07-22 17:55:18 -0300

    feat(domain): adiciona entidades RecipeDishType, RecipeIngredient e RecipeInstruction

---
+++ src/Backend/MyRecipeBook.Domain/Entities/RecipeIngredient.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Domain.Entities;
+
+public class RecipeIngredient: EntityBase
+{
+    public string Item { get; set; } = string.Empty;
+
+    public Guid RecipeId { get; private set; }
+}
