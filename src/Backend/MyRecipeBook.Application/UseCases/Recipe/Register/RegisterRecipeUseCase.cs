b9911a6f8c28b4aa6a5e2766d4e419aa63c537e6
Mateus <matt_ufo2@hotmail.com>
2026-07-24 17:57:49 -0300

    feat(recipes): implementa use case e validator para registro de receitas
    - Adicionado RegisterRecipeUseCase para gerenciar o fluxo de cadastro de receitas vinculado ao usuário logado
    - Criado RecipeValidator utilizando FluentValidation para garantir as regras de validação de título, tempo de preparo, tipos de prato, ingredientes e instruções
    - Definida a interface IRegisterRecipeUseCase para o contrato de uso

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Register/RegisterRecipeUseCase.cs
@@ -0,0 +1,53 @@
+﻿using Mapster;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Register;
+
+public class RegisterRecipeUseCase : IRegisterRecipeUseCase
+{
+    private readonly IRecipeWriteOnlyRepository _repository;
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterRecipeUseCase(IRecipeWriteOnlyRepository repository, ILoggedUser loggedUser, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _loggedUser = loggedUser;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ResponseRegisterRecipeJson> Execute(RequestRecipeJson request)
+    {
+        Validate(request);
+
+        var recipe = request.Adapt<MyRecipeBook.Domain.Entities.Recipe>();
+
+        recipe.UserId = _loggedUser.GetUserId();
+
+        await _repository.Add(recipe);
+
+        await _unitOfWork.Commit();
+
+        return new ResponseRegisterRecipeJson
+        {
+            Id = recipe.Id,
+            Title = recipe.Title
+        };
+
+    }
+
+    public void Validate(RequestRecipeJson request)
+    {
+        var result = new RecipeValidator().Validate(request);
+
+        if (result.IsValid == false)
+        {
+            throw new ErrorOnValidationException(result.Errors.Select(e => e.ErrorMessage).ToList());
+        }
+    }
+}
