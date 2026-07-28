b7645bbd3492706a211ae50aa6d07d2fc5664569
Mateus <matt_ufo2@hotmail.com>
2026-07-28 17:59:39 -0300

    refactor(infrastructure): corrige o tipo no nome do método AddLoggeedUser para AddLoggedUser

--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Domain.Identity;
 using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Domain.Security.PasswordHashing;
 using MyRecipeBook.Domain.Security.Tokens;
@@ -30,7 +31,7 @@ public static class DependencyInjectionExtension
 
             services.AddDbContext(configuration);
 
-            services.AddLogeedUser();
+            services.AddLoggedUser();
         }
 
         private void AddRepositories()
@@ -42,6 +43,8 @@ public static class DependencyInjectionExtension
             services.AddScoped<IUserUpdateOnlyRepository, UserRepository>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IRecipeWriteOnlyRepository, RecipeRepository>();
         }
 
         private void AddTokensHandlers(IConfiguration configuration)
@@ -84,7 +87,7 @@ public static class DependencyInjectionExtension
             services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
         }
 
-        private void AddLogeedUser()
+        private void AddLoggedUser()
         {
             services.AddScoped<ILoggedUser, LoggedUser>();
         }
