d5537f5d0eebe6b4b87bdacdfc9ef0be09655e56
Mateus <matt_ufo2@hotmail.com>
2026-07-29 18:00:05 -0300

    feat(infrastructure): implementa RecipeRepository com suporte a escrita de receitas

---
+++ src/Backend/MyRecipeBook.Infrastructure/Repositories/RecipeRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Infrastructure.DataAcess;
+
+namespace MyRecipeBook.Infrastructure.Repositories;
+
+internal sealed class RecipeRepository: IRecipeWriteOnlyRepository
+{
+    private readonly MyRecipeBookDbContext _dbContext;
+
+    public RecipeRepository(MyRecipeBookDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Add(Recipe recipe)
+    {
+        await _dbContext.AddAsync(recipe);
+    }
+}
