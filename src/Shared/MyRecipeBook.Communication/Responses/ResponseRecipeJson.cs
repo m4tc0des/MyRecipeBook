22fa463908f3e8de93ae24f271f9c4c457947eaa
Mateus <matt_ufo2@hotmail.com>
2026-08-04 12:09:11 -0300

    feat: adicionar classes de resposta (DTOs) para receitas e instruções
    - Cria a classe ResponseInstructionJson contendo as propriedades de ordem e descrição para os passos da receita.
    - Cria/atualiza a classe ResponseRecipeJson para estruturar o contrato de resposta da API de receitas, incluindo ID, título, ingredientes, instruções, tipos de prato e tempo de preparo.

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRecipeJson.cs
@@ -0,0 +1,17 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Responses;
+
+public class ResponseRecipeJson
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public IList<string> Ingredients { get; set; } = new List<string>();
+
+    public IList<ResponseInstructionJson> Instructions { get; set; } = new List<ResponseInstructionJson>();
+
+    public IList<DishTypes> DishTypes { get; set; } = new List<DishTypes>();
+
+    public CookTime CookTime { get; set; }
+}
