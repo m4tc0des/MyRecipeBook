dd98b5d8655a8230374a070cb0e6a5446f60e047
Mateus <matt_ufo2@hotmail.com>
2026-08-08 11:26:50 -0300

    feat(application): implementar caso de uso GetRecipeByIdUseCase
    - Injetar IRecipeReadOnlyRepository e ILoggedUser no construtor
    - Buscar receita validando o ID e o ID do usuário logado
    - Lançar NotFoundException caso a receita não seja encontrada
    - Mapear a entidade Recipe para ResponseRecipeJson utilizando Mapster
    Adicionar contrato IRecipeReadOnlyRepository na camada de repositórios de receita
    - Definir método GetById assíncrono para buscar receita por ID validando o ID do usuário

--- src/Backend/MyRecipeBook.Application/UseCases/Recipe/GetById/GetRecipeByIdUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/GetById/GetRecipeByIdUseCase.cs
@@ -1,11 +1,32 @@
-﻿using MyRecipeBook.Communication.Responses;
+﻿using Mapster;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
 
 namespace MyRecipeBook.Application.UseCases.Recipe.GetById;
 
 public class GetRecipeByIdUseCase : IGetRecipeByIdUseCase
 {
-    public Task<ResponseRecipeJson> Execute(Guid recipeId)
+    private readonly IRecipeReadOnlyRepository _recipeReadOnlyRepository;
+    private readonly ILoggedUser _loggedUser;
+
+    public GetRecipeByIdUseCase(IRecipeReadOnlyRepository recipeReadOnlyRepository, ILoggedUser loggedUser)
     {
-        throw new NotImplementedException();
+        _recipeReadOnlyRepository = recipeReadOnlyRepository;
+        _loggedUser = loggedUser;
+    }
+
+    public async Task<ResponseRecipeJson> Execute(Guid recipeId)
+    {
+        var recipe = await _recipeReadOnlyRepository.GetById(recipeId, _loggedUser.GetUserId());
+
+        if (recipe is null)
+        {
+            throw new NotFoundException(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        }
+
+        return recipe.Adapt<ResponseRecipeJson>();
     }
 }
