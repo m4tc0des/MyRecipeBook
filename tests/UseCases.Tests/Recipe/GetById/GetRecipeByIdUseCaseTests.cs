93eda8acc3557ceafade9bec84858769d759402b
Mateus <matt_ufo2@hotmail.com>
2026-08-10 17:36:39 -0300

    test(application): implementar testes unitários para GetRecipeByIdUseCase
    - Criar a classe GetRecipeByIdUseCaseTests cobrindo os cenários de sucesso e exceção
    - Validar o mapeamento correto dos dados da receita e a ordenação ascendente das instruções no teste de sucesso
    - Garantir o lançamento de NotFoundException com a mensagem correta quando a receita não for encontrada
    - Utilizar os builders utilitários para isolar dependências (UserBuilder, RecipeBuilder, ILoggedUserBuilder e IRecipeReadOnlyRepositoryBuilder)

---
+++ tests/UseCases.Tests/Recipe/GetById/GetRecipeByIdUseCaseTests.cs
@@ -0,0 +1,65 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Identity;
+using CommonTestUtilities.Repositories;
+using MyRecipeBook.Application.Mappings;
+using MyRecipeBook.Application.UseCases.Recipe.GetById;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+using Shouldly;
+
+namespace UseCases.Tests.Recipe.GetById;
+
+public class GetRecipeByIdUseCaseTests
+{
+    static GetRecipeByIdUseCaseTests()
+    {
+        MapsterConfiguration.Configure();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var recipe = RecipeBuilder.Build(user);
+
+        var useCase = CreateUseCase(recipe, user);
+
+        var result = await useCase.Execute(recipe.Id);
+
+        result.ShouldNotBeNull();
+
+        result.Id.ShouldBe(recipe.Id);
+
+        result.Title.ShouldBe(recipe.Title);
+
+        result.Instructions.Select(x => x.Order).ShouldBeInOrder(SortDirection.Ascending);
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_When_RecipeNotFound()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var recipe = RecipeBuilder.Build(user);
+
+        var useCase = CreateUseCase(recipe, user);
+
+        var exception = await useCase.Execute(Guid.CreateVersion7()).ShouldThrowAsync<NotFoundException>();
+
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessages =>
+        {
+            errorMessages.Count.ShouldBe(1);
+            errorMessages.ShouldContain(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        });
+    }
+
+    private static GetRecipeByIdUseCase CreateUseCase(MyRecipeBook.Domain.Entities.Recipe recipe, MyRecipeBook.Domain.Entities.User user)
+    {
+        var loggedUser = ILoggedUserBuilder.Build(user);
+
+        var repository = new IRecipeReadOnlyRepositoryBuilder().GetById(recipe).Build();
+
+        return new GetRecipeByIdUseCase(repository, loggedUser);
+    }
+}
