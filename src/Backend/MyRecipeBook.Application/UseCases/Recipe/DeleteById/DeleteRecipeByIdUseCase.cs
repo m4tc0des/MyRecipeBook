779179d43c1610dac655f820a95e6d750d643995
Mateus <matt_ufo2@hotmail.com>
2026-08-12 09:51:52 -0300

    feat(application): implementar DeleteRecipeByIdUseCase para exclusão de receitas
    - Criar a classe DeleteRecipeByIdUseCase implementando a interface IDeleteRecipeByIdUseCase
    - Executar a remoção da receita no repositório de escrita utilizando o ID da receita e o ID do usuário logado
    - Lançar NotFoundException com a mensagem adequada caso a exclusão falhe por receita não encontrada
    - Registrar o novo use case como escopado no container de Injeção de Dependências

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/DeleteById/DeleteRecipeByIdUseCase.cs
@@ -0,0 +1,28 @@
+﻿using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.DeleteById;
+
+public class DeleteRecipeByIdUseCase: IDeleteRecipeByIdUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+
+    private readonly IRecipeWriteOnlyRepository _recipeWriteOnlyRepository;
+
+    public DeleteRecipeByIdUseCase(IRecipeWriteOnlyRepository recipeWriteOnlyRepository, ILoggedUser loggedUser)
+    {
+        _recipeWriteOnlyRepository = recipeWriteOnlyRepository;
+        _loggedUser = loggedUser;
+    }
+
+    public async Task Execute(Guid recipeId)
+    {
+        var deleted = await _recipeWriteOnlyRepository.DeleteById(recipeId, _loggedUser.GetUserId());
+        if (deleted == false)
+        {
+            throw new NotFoundException(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        }
+    }
+}
