08ad9a3cf1490a5cd44687c2c661521f10909044
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-08-13 17:55:32 -0300

    test(usecases): adiciona testes unitários para DeleteRecipeByIdUseCase

---
+++ tests/UseCases.Tests/Recipe/DeleteById/DeleteRecipeByIdUseCaseTests.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Identity;
+using CommonTestUtilities.Repositories;
+using MyRecipeBook.Application.UseCases.Recipe.DeleteById;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+using Shouldly;
+
+namespace UseCases.Tests.Recipe.DeleteById;
+
+public class DeleteRecipeByIdUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var recipe = RecipeBuilder.Build(user);
+
+        var useCase = CreateUseCase(recipe, user);
+
+        await useCase.Execute(recipe.Id).ShouldNotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_When_RecipeNotFound()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var recipe = RecipeBuilder.Build(user);
+
+        var useCase = CreateUseCase(recipe, user);
+
+        var exception = await useCase.Execute(Guid.CreateVersion7()).ShouldThrowAsync<NotFoundException>();
+
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessage =>
+        {
+            errorMessage.Count.ShouldBe(1);
+            errorMessage.ShouldContain(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        });
+    }
+
+    private static DeleteRecipeByIdUseCase CreateUseCase(MyRecipeBook.Domain.Entities.Recipe recipe, MyRecipeBook.Domain.Entities.User user)
+    {
+        var loggedUser = ILoggedUserBuilder.Build(user);
+
+        var repository = new IRecipeWriteOnlyRepositoryBuilder().DeleteById(recipe).Build();
+
+        return new DeleteRecipeByIdUseCase(repository, loggedUser);
+    }
+}
