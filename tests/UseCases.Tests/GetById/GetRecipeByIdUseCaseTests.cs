051fc19abbb2c2a556b747df13df82583a60afbb
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-08-13 17:58:08 -0300

    test(usecases): estrutura classe de testes para GetRecipeByIdUseCase

---
+++ tests/UseCases.Tests/GetById/GetRecipeByIdUseCaseTests.cs
@@ -0,0 +1,23 @@
+﻿using MyRecipeBook.Application.Mappings;
+
+namespace UseCases.Tests.GetById;
+
+public  class GetRecipeByIdUseCaseTests
+{
+    public GetRecipeByIdUseCaseTests()
+    {
+        MapsterConfiguration.Configure();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_When_RecipeNotFound()
+    {
+        
+    }
+}
