f9478ac836466700d5940db03a40708048dfeb21
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-08-17 16:19:38 -0300

    feat: implementar caso de uso de atualização de receita por ID

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/UpdateById/UpdateRecipeByIdUseCase.cs
@@ -0,0 +1,49 @@
+﻿using Mapster;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.UpdateById;
+
+public class UpdateRecipeByIdUseCase : IUpdateRecipeByIdUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IRecipeUpdateOnlyRepository _recipeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateRecipeByIdUseCase(ILoggedUser loggedUser, IRecipeUpdateOnlyRepository recipeRepository, IUnitOfWork unitOfWork)
+    {
+        _loggedUser = loggedUser;
+        _recipeRepository = recipeRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(Guid recipeId, RequestRecipeJson request)
+    {
+        ValidateAndThrowOnFailures(request);
+
+        var recipe = await _recipeRepository.GetById(recipeId, _loggedUser.GetUserId());
+
+        if (recipe is null)
+        {
+            throw new NotFoundException(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        }
+
+        request.Adapt(recipe);
+
+        await _unitOfWork.Commit();
+    }
+
+    public static void ValidateAndThrowOnFailures(RequestRecipeJson request)
+    {
+        var result = new RecipeValidator().Validate(request);
+
+        if (result.IsValid == false)
+        {
+            throw new ErrorOnValidationException(result.Errors.Select(error => error.ErrorMessage).ToList());
+        }
+    }
+}
