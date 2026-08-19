2c8d096475ff57443356d8f3ac660ff013cd5c04
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-08-19 17:09:27 -0300

    test(integration): implementa testes de segurança e autenticação para o endpoint de update de receitas
    - Adicionado teste para token de acesso inválido (retorna Unauthorized).
    - Adicionado teste para ausência de token de acesso (retorna Unauthorized).
    - Adicionado teste para token de usuário inexistente no banco de dados (retorna Unauthorized).

---
+++ tests/WebApi.Tests/Recipe/UpdateById/UpdateRecipeInvalidTokenTests.cs
@@ -0,0 +1,43 @@
+﻿using MyRecipeBook.Communication.Requests;
+using Shouldly;
+using System.Net;
+
+namespace WebApi.Tests.Recipe.UpdateById;
+
+public class UpdateRecipeInvalidTokenTests: BaseIntegrationTest
+{
+    private const string REQUEST_URI = "/recipes";
+    private readonly string _tokenUserNotExistDatabase;
+
+    public UpdateRecipeInvalidTokenTests(MyRecipeBookApplicationFactory factory): base(factory)
+    {
+        _tokenUserNotExistDatabase = factory.TOKEN_USER_NOT_FOUND_IN_DATABASE;
+    }
+
+    [Fact]
+    public async Task Validate_ShouldBeAnErrorResponse_When_AccessTokenIsInvalid()
+    {
+        var request = new RequestRecipeJson();
+        var response = await Put($"{REQUEST_URI}/{Guid.CreateVersion7()}", request, accessToken: "tokenInvalid");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Validate_ShouldBeAnErrorResponse_When_AccessTokenIsMissing()
+    {
+        var request = new RequestRecipeJson();
+        var response = await Put($"{REQUEST_URI}/{Guid.CreateVersion7()}", request, accessToken: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Validate_ShouldBeAnErrorResponse_When_AccessTokenDoesNotExist()
+    {
+        var request = new RequestRecipeJson();
+        var response = await Put($"{REQUEST_URI}/{Guid.CreateVersion7()}", request, accessToken: _tokenUserNotExistDatabase);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
