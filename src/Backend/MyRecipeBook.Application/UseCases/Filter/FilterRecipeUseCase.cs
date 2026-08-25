e0a73c6baf59615bc667e642dfb98939299aaab2
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-08-25 09:53:10 -0300

    feat(recipes): implementa use case e request para filtragem de receitas
    - Criada a classe RequestFilterRecipeJson com os parâmetros de filtro (SearchItem, CookTime e DishTypes).
    - Implementada a classe FilterRecipeUseCase injetando ILoggedUser e IRecipeReadOnlyRepository, com o método Execute configurado como stub.

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Filter/FilterRecipeUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories.Recipe;
+
+namespace MyRecipeBook.Application.UseCases.Filter;
+
+public class FilterRecipeUseCase : IFilterRecipeUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IRecipeReadOnlyRepository _recipeReadRepository;
+
+    public FilterRecipeUseCase(ILoggedUser loggedUser, IRecipeReadOnlyRepository recipeReadRepository)
+    {
+        _loggedUser = loggedUser;
+        _recipeReadRepository = recipeReadRepository;
+    }
+    public Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson? request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestFilterRecipeJson.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Requests;
+
+public class RequestFilterRecipeJson
+{
+    public string? SearchItem { get; set; }
+    public CookTime? CookTime { get; set; }
+    public IList<DishTypes> DishTypes { get; set; } = [];
+}
