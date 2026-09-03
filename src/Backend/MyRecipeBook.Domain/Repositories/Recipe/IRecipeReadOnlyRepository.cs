d22820fede39dd6aa3ea51b54f40d6961ebd7627
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-09-03 17:51:29 -0300

    feat(domain): adiciona RecipeSummaryDto e atualiza assinaturas em IRecipeReadOnlyRepository
    - Criado o record RecipeSummaryDto no namespace MyRecipeBook.Domain.Dtos para representar o sumário de uma receita (Id e Título).
    - Renomeado e ajustado o contrato GetRecentRecipe para GetRecentRecipes na interface IRecipeReadOnlyRepository, passando a retornar IList<RecipeSummaryDto>.
    - Atualizado o método FilterRecipes em IRecipeReadOnlyRepository para receber o RecipeFilterDto junto ao ID do usuário.

--- src/Backend/MyRecipeBook.Domain/Repositories/Recipe/IRecipeReadOnlyRepository.cs
+++ src/Backend/MyRecipeBook.Domain/Repositories/Recipe/IRecipeReadOnlyRepository.cs
@@ -1,8 +1,11 @@
-﻿namespace MyRecipeBook.Domain.Repositories.Recipe;
+﻿using MyRecipeBook.Domain.Dtos;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Domain.Repositories.Recipe;
 
 public interface IRecipeReadOnlyRepository
 {
     Task<Entities.Recipe?> GetById(Guid recipeId, Guid userId);
-    Task<IList<Entities.Recipe>> GetRecentRecipe(Guid userId);
-    Task<IList<Entities.Recipe>> FilterRecipes(Guid userId);
+    Task<IList<RecipeSummaryDto>> GetRecentRecipes(Guid userId);
+    Task<IList<Entities.Recipe>> FilterRecipes(Guid userId, RecipeFilterDto filter);
 }
