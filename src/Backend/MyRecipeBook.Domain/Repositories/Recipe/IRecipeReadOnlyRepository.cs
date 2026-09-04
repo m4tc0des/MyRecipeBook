49f823d67aa8aba6b777e6eda9df7d5112acc6d1
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-09-04 17:34:12 -0300

    refactor(domain): atualiza o tipo de retorno de FilterRecipes para RecipeSummaryDto
    - Alterada a assinatura do método FilterRecipes na interface IRecipeReadOnlyRepository para retornar IList<RecipeSummaryDto> em vez de IList<Entities.Recipe>.

--- src/Backend/MyRecipeBook.Domain/Repositories/Recipe/IRecipeReadOnlyRepository.cs
+++ src/Backend/MyRecipeBook.Domain/Repositories/Recipe/IRecipeReadOnlyRepository.cs
@@ -7,5 +7,5 @@ public interface IRecipeReadOnlyRepository
 {
     Task<Entities.Recipe?> GetById(Guid recipeId, Guid userId);
     Task<IList<RecipeSummaryDto>> GetRecentRecipes(Guid userId);
-    Task<IList<Entities.Recipe>> FilterRecipes(Guid userId, RecipeFilterDto filter);
+    Task<IList<RecipeSummaryDto>> FilterRecipes(Guid userId, RecipeFilterDto filter);
 }
