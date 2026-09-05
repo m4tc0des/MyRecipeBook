add845a3becec3d1ef698f551aed7e45df9bd6ba
Mateus <118913697+m4tc0des@users.noreply.github.com>
2026-09-05 09:15:18 -0300

    feat(application): implementa RequestPasswordRecoveryCodeUseCase para geração de código de recuperação de senha
    - Criada a classe RequestPasswordRecoveryCodeUseCase implementando a interface IRequestPasswordRecoveryCodeUseCase.
    - Implementado o método Execute para verificar se o usuário existe por e-mail, gerar um código numérico aleatório de 6 dígitos formatado e persistir a entidade VerificationCode no banco de dados via repositório e UnitOfWork.

---
+++ src/Backend/MyRecipeBook.Application/UseCases/RequestCode/RequestPasswordRecoveryCodeUseCase.cs
@@ -0,0 +1,43 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Enums;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Domain.Repositories.VerificationCode;
+using System.Security.Cryptography;
+
+namespace MyRecipeBook.Application.UseCases.RequestCode;
+
+public class RequestPasswordRecoveryCodeUseCase : IRequestPasswordRecoveryCodeUseCase
+{
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IVerificationCodeWriteOnlyRepository _verificationCodeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RequestPasswordRecoveryCodeUseCase(IUserReadOnlyRepository userReadOnlyRepository, IVerificationCodeWriteOnlyRepository verificationCodeRepository, IUnitOfWork unitOfWork)
+    {
+        _userReadOnlyRepository = userReadOnlyRepository;
+        _verificationCodeRepository = verificationCodeRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(RequestPasswordRecoveryJson request)
+    {
+        var user = await _userReadOnlyRepository.GetByEmail(request.Email);
+
+        if (user is null) return;
+
+        var code = RandomNumberGenerator.GetInt32(1, 1_000_000);
+
+        var verificationCode = new VerificationCode
+        {
+            Code = code.ToString("D6"),
+            Type = VerificationCodeType.PasswordRecovery,
+            UserId = user.Id,
+        };
+
+        await _verificationCodeRepository.Add(verificationCode);
+        await _unitOfWork.Commit();
+    }
+}
